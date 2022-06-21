ec3cc5f5aef3d10c695739d413d36d213f5b83c6
Paul-Emmanuel Chouc <pechouc@gmail.com>
2022-06-21 15:39:38 +0200

    Adding the option to download the data

--- fetch_data.sh
+++ fetch_data.sh
@@ -1,7 +1,7 @@
 curl http://www.cepii.fr/DATA_DOWNLOAD/baci/data/BACI_HS12_V202201.zip > data/BACI_HS12_V202201.zip
-unzip data/BACI_HS12_V202201.zip
+unzip data/BACI_HS12_V202201.zip -d data/BACI_HS12_V202201
 
 curl http://www.cepii.fr/DATA_DOWNLOAD/gravity/data/Gravity_csv_V202202.zip > data/Gravity_csv_V202202.zip
-unzip data/Gravity_csv_V202202.zip
+unzip data/Gravity_csv_V202202.zip -d data/Gravity_csv_V202202
 
-curl http://stats.oecd.org/SDMX-JSON/data/CBCR_TABLEI/ALL/OECD?contenttype=csv > data/oecd_cbcr.csv
+curl https://stats.oecd.org/SDMX-JSON/data/CBCR_TABLEI/ALL/OECD?contenttype=csv > data/oecd_cbcr.csv
