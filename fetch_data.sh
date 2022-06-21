adb774649bed306fe76ab54081a4528420952dee
Paul-Emmanuel <63041127+pechouc@users.noreply.github.com>
2022-06-21 15:08:29 +0200

    Add files via upload

---
+++ fetch_data.sh
@@ -0,0 +1,7 @@
+curl http://www.cepii.fr/DATA_DOWNLOAD/baci/data/BACI_HS12_V202201.zip > data/BACI_HS12_V202201.zip
+unzip data/BACI_HS12_V202201.zip
+
+curl http://www.cepii.fr/DATA_DOWNLOAD/gravity/data/Gravity_csv_V202202.zip > data/Gravity_csv_V202202.zip
+unzip data/Gravity_csv_V202202.zip
+
+curl http://stats.oecd.org/SDMX-JSON/data/CBCR_TABLEI/ALL/OECD?contenttype=csv > data/oecd_cbcr.csv
